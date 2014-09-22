50dd7a851a29971d24afd0ef222aeadf9cffb4e4
lanatra <Anden702@gmail.com>
2014-09-22 10:58:26 +0200

    Multiple user history

--- GameEngine.java
+++ GameEngine.java
@@ -24,19 +24,22 @@ public class GameEngine {
                 gTurn.main();
             }
     	}
-    	// int[] games = Player.getWinLoses();
-    	// int[][] rolls = Player.getDistrRolls();
-    	// int[] max = {0,0}; //Face, Count
-    	// int[] min = {0,1000}; //Face, Count
-    	// for (int i = 0; i < rolls.length ; i++) {
-    	// 		if(rolls[i][1] > max[1]) max[0] = rolls[i][0];
-    	// 		if(rolls[i][1] < min[1] && rolls[i][1] > 0) min[0] = rolls[i][0];
-    	// }
-    	// String infoMessage = "Final score:" + Player.getBank() + "\n" +
-    	// "You played " + (games[0] + games[1]) + " games with a win ratio of " + Player.getWinRate() + " (W" + games[0] + "/L" + games[1] + ")\n" + 
-    	// "Your average bet over " + Player.getBets().length + " bets was " + Player.getAveBet() + "\n" + 
-    	// "You rolled " + max[0] + "'s the most, and " + min[0] + "'s the least with an average sum of " + Player.getAveRoll();
-    	// JOptionPane.showMessageDialog(null, infoMessage, "Game Statistics", JOptionPane.INFORMATION_MESSAGE);
+        for (int i = 0; i < playerCount ; i++) {
+            Player player = Players.get(i);
+            int[] games = player.getWinLoses();
+            int[][] rolls = player.getDistrRolls();
+            int[] max = {0,0}; //Face, Count
+            int[] min = {0,1000}; //Face, Count
+            for (int j = 0; j < rolls.length ; j++) {
+                if(rolls[j][1] > max[1]) max[0] = rolls[j][0];
+                if(rolls[j][1] < min[1] && rolls[j][1] > 0) min[0] = rolls[j][0];
+            }
+            String infoMessage = "Final score:" + player.getBank() + "\n" +
+            "You played " + (games[0] + games[1]) + " games with a win ratio of " + player.getWinRate() + " (W" + games[0] + "/L" + games[1] + ")\n" + 
+            "Your average bet over " + player.getBets().length + " bets was " + player.getAveBet() + "\n" + 
+            "You rolled " + max[0] + "'s the most, and " + min[0] + "'s the least with an average sum of " + player.getAveRoll();
+            JOptionPane.showMessageDialog(null, infoMessage, "Game Statistics", JOptionPane.INFORMATION_MESSAGE);
+        }
     }
     public boolean playersAlive() {
         int count = 0;
